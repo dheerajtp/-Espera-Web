bb4a8e856a2f4d37f5aa78de770904a02811ff72
dheerajtp <dheerajtp6338@gmail.com>
2023-03-14 05:40:04 +0530

    :lipstick: Cart Page Design Fixes

--- src/components/Cart/MyCartItem.jsx
+++ src/components/Cart/MyCartItem.jsx
@@ -3,8 +3,7 @@ import {
   Box,
   Grid,
   Card,
-  CardActions,
-  CardContent,
+  CardHeader,
   CardMedia,
   IconButton,
   Typography,
@@ -15,48 +14,63 @@ import { BASE_URL } from "../../configuration";
 function MyCartItem({ quantity, pr_name, con_win, image, pr_price }) {
   return (
     <Grid item xs={12} sm={6} md={3} lg={3}>
-      <Card sx={{ maxWidth: 345 }}>
-        <Typography variant="h4" m={1}>
-          {pr_name}
-        </Typography>
+      <Card
+        sx={{
+          borderRadius: 4,
+          border: "2px solid green",
+          borderWidth: 4,
+        }}
+      >
+        <CardHeader title={pr_name} />
         <CardMedia
           component="img"
-          height="194"
+          height="150"
           image={`${BASE_URL}images/contest_cover/${image}`}
-          alt={pr_name}
+          alt="Product Image"
         />
-        <CardContent>
-          <Typography variant="h6" color="text.secondary" m={2}>
-            WIN {con_win}
-          </Typography>
-          <Box sx={{ display: "flex", gap: ".2rem" }}>
-            <Typography
-              variant="h6"
-              color="text.secondary"
-              m={2}
-              fontWeight="bold"
-            >
-              {pr_price}
-            </Typography>
-            <Typography variant="h6" color="text.secondary" m={2}>
-              USD
-            </Typography>
-          </Box>
-        </CardContent>
-        <CardActions sx={{ display: "flex", flexDirection: "column" }}>
-          <Typography>Quantity</Typography>
-          <Box sx={{ display: "flex", alignItems: "center" }}>
-            <IconButton>
-              <Remove />
-            </IconButton>
-            <Typography variant="subtitle1" component="div">
-              {quantity}
-            </Typography>
-            <IconButton>
-              <Add />
-            </IconButton>
-          </Box>
-        </CardActions>
+        <Grid container spacing={2} alignItems="center" justifyContent="center">
+          <Grid item xs={12} sx={{ padding: "16px" }}>
+            <Grid container spacing={0.5} alignItems="center">
+              <Grid item xs={6} sx={{ padding: "8px" }}>
+                <Typography variant="p">WIN {con_win}</Typography>
+              </Grid>
+              <Grid item xs={6}>
+                <Grid container spacing={0.5} alignItems="center">
+                  <Grid item xs={12}>
+                    <Typography variant="h6" align="center">
+                      {pr_price}.0 USD
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Typography variant="body2" align="center">
+                      Quantity
+                    </Typography>
+                  </Grid>
+                  <Grid item xs={12}>
+                    <Box
+                      sx={{
+                        display: "flex",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        gap: 2,
+                      }}
+                    >
+                      <IconButton>
+                        <Remove />
+                      </IconButton>
+                      <Typography variant="P" align="center">
+                        {quantity}
+                      </Typography>
+                      <IconButton>
+                        <Add />
+                      </IconButton>
+                    </Box>
+                  </Grid>
+                </Grid>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
       </Card>
     </Grid>
   );
